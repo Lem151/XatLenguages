5751e9dfc66570c125b9ca9017e2e0772a1bb29a
Elm <114771568+Lem151@users.noreply.github.com>
2023-06-05 11:01:04 +0200

    Add files via upload

--- ProjectoFinal/scriptChat.js
+++ ProjectoFinal/scriptChat.js
@@ -1,3 +1,6 @@
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const friendEmail = sessionStorage.getItem('friendEmail');
 
@@ -14,29 +17,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const email = sessionStorage.getItem('email');
     const session = sessionStorage.getItem('session');
     const receiver = friendEmail;
-
+    //no enviamos mensajes vacios y asi no saturamos la BD
+    if(message != "") {
+      $.ajax({
+        url: 'http://localhost:6900/XatLenguajes/Xat',
+        type: 'POST',
+        data: {
+          mail: email,
+          session: session,
+          receptor: receiver,
+          sms: message
+        },
+        success: function(response) {
+          // Realizar cualquier acción adicional después de enviar el mensaje
+          console.log('Mensaje enviado:', message);
+  
+          // Mostrar el mensaje enviado en el chat
+          recibirMensajes(email, session); // Llamada para recibir mensajes después de enviar uno
+          displayMessage(email, message);
+  
+        },
+        error: function(xhr, status, error) {
+          console.error('Error al enviar mensaje:', error);
+        }
+      });
+    }
     // Realizar la solicitud POST a través de AJAX
-    $.ajax({
-      url: 'http://localhost:6900/XatLenguajes/Xat',
-      type: 'POST',
-      data: {
-        mail: email,
-        session: session,
-        receptor: receiver,
-        sms: message
-      },
-      success: function(response) {
-        // Realizar cualquier acción adicional después de enviar el mensaje
-        console.log('Mensaje enviado:', message);
-
-        // Mostrar el mensaje enviado en el chat
-        displayMessage(email, message);
-        recibirMensajes(email, session); // Llamada para recibir mensajes después de enviar uno
-      },
-      error: function(xhr, status, error) {
-        console.error('Error al enviar mensaje:', error);
-      }
-    });
 
     messageInput.value = '';
   }
@@ -48,9 +54,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     chatMessages.appendChild(messageElement);
   }
-
+  //Recive mensajes de otros pero no los muestra si no son la persona con la que chateas, aun asi los borra de la base de datos por un fallo del backend 
   function recibirMensajes(mail, session) {
     // Realizar la solicitud GET a través de AJAX
+    console.log("entra en la funcion")
     $.ajax({
       url: 'http://localhost:6900/XatLenguajes/Xat',
       type: 'GET',
@@ -58,17 +65,24 @@ document.addEventListener('DOMContentLoaded', () => {
         mail: mail,
         session: session
       },
+      
       success: function(response) {
-        // Verificar si se recibió un mensaje válido
-        if (response.emisor && response.text) {
-          // Mostrar el mensaje recibido en el chat
-          displayMessage(response.emisor, response.text);
-          console.log(response);
+        var res = JSON.parse(response);
+        console.log(res.text)
+        console.log(res.emisor, res.text);
+  
+        //Mira si hay info en el mensaje
+        if(res.text != "" && res.emisor == friendEmail) {
+          //Muestra el mensaje 
+          displayMessage(res.emisor, res.text);
         }
+
       },
-      error: function(xhr, status, error) {
-        console.error('Error al recibir mensajes:', error);
+      error: function(xhr, status, errorThrown) {
+        console.error('Error al recibir mensajes:', errorThrown);
       }
     });
+    
   }
 });
+
